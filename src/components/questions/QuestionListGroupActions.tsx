de590669a15e7ba928c4e8fb564cb805e00cacfa
matt765 <mateusz.wyrebek@gmail.com>
2024-07-17 21:13:00 +0200

    Add estimated time display

--- src/components/questions/QuestionListGroupActions.tsx
+++ src/components/questions/QuestionListGroupActions.tsx
@@ -1,6 +1,7 @@
 import { QuestionCategory, useQuestionStore } from "@/store/questionStore";
-import styles from "./styles/QuestionList.module.scss";
+import styles from "./styles/QuestionListGroupActions.module.scss";
 import { OutlinedButton } from "../common/OutlinedButton";
+import { ClockIcon } from "@/assets/icons/ClockIcon";
 
 interface QuestionListGroupActionsProps {
   questionCategory: QuestionCategory;
@@ -10,18 +11,38 @@ export const QuestionListGroupActions = ({
   questionCategory,
 }: QuestionListGroupActionsProps) => {
   const resetCheckboxes = useQuestionStore((state) => state.resetCheckboxes);
+  const estimatedTime = estimatedTimes[questionCategory];
 
   return (
-    <div className={styles.questionListGroupActionsWrapper}>
-      <div className={styles.questionListGroupActionsButtonWrapper}>
-        <OutlinedButton
-          text="Reset checkboxes"
-          onClick={() => resetCheckboxes(questionCategory)}
-        />
+    <div className={styles.groupActionsWrapper}>
+      <div className={styles.estimatedTimeWrapper}>
+        <div className={styles.estimatedTimeIcon}>
+          <ClockIcon />
+        </div>
+        Estimated time: {estimatedTime} hours
       </div>
-      <div className={styles.questionListGroupActionsButtonWrapper}>
-        <OutlinedButton text="Export selected to PDF" onClick={() => {}} />
+      <div className={styles.groupActionsButtonsWrapper}>
+        <div className={styles.groupActionsButtonWrapper}>
+          <OutlinedButton
+            text="Reset checkboxes"
+            onClick={() => resetCheckboxes(questionCategory)}
+          />
+        </div>
+        <div className={styles.groupActionsButtonWrapper}>
+          <OutlinedButton text="Export selected to PDF" onClick={() => {}} />
+        </div>
       </div>
     </div>
   );
+};
+
+export const estimatedTimes: Record<QuestionCategory, number> = {
+  HTML: 15,
+  CSS: 20,
+  JavaScript: 45,
+  TypeScript: 12,
+  React: 20,
+  Git: 8,
+  Optimization: 5,
+  General: 15,
 };
